c52ab040c129b4c8f19a47e42ec4c5d26acc618d
tibs <git@tibinonest.me>
2023-05-19 22:53:53 -0400

    Add sitemap

--- _config.ts
+++ _config.ts
@@ -3,10 +3,12 @@ import nav from "lume/plugins/nav.ts";
 import sass from "lume/plugins/sass.ts";
 import lightningCss from "lume/plugins/lightningcss.ts";
 import terser from "lume/plugins/terser.ts";
+import sitemap from "lume/plugins/sitemap.ts";
 import minifyHTML from "lume/plugins/minify_html.ts";
 
 const site = lume({
 	src: "./src",
+	location: new URL("https://tibinonest.me"),
 	server: {
 		page404: "404/"
 	}
@@ -16,6 +18,7 @@ site.use(nav());
 site.use(sass());
 site.use(lightningCss());
 site.use(terser());
+site.use(sitemap({ query: "index!=false" }));
 site.use(minifyHTML({
 	options: {
 		do_not_minify_doctype: true,
